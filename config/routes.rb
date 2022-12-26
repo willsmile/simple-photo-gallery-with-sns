680935278cb6db5133de079890c53133e244551e
Wei Chen <smile.v.chen@gmail.com>
2022-12-26 20:38:16 +0900

    写真アップデート・写真一覧機能の実装の準備として不要なファイルとコードの削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'photos#index'
 
-  resources :photos
+  resources :photos, only: [:new, :create, :index]
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
