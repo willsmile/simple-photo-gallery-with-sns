e514a23292f2ee775c3aabb099a81be6deed6fa0
Wei Chen <smile.v.chen@gmail.com>
2022-12-26 20:48:22 +0900

    Photoモデルに写真のvariantの設定を追加

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,8 @@
 class Photo < ApplicationRecord
   belongs_to :user
-  has_one_attached :image_file
+  has_one_attached :image_file do |attachable|
+    attachable.variant :thumb, resize_to_limit: [200, 200]
+  end
 
   validates :title, presence: { message: 'タイトルを入力してください' },
   length: { maximum: 30, too_long: "タイトルは最大%{count}文字まで使えます" }
