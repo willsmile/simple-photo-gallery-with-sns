4cc64ef984e67fa739ae6bd755816620445983e1
Wei Chen <smile.v.chen@gmail.com>
2022-12-26 20:40:10 +0900

    Photoモデル必要なバリデーションの追加とUserモデルとのアソシエーションの追加

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,9 @@
 class Photo < ApplicationRecord
   belongs_to :user
   has_one_attached :image_file
+
+  validates :title, presence: { message: 'タイトルを入力してください' },
+  length: { maximum: 30, too_long: "タイトルは最大%{count}文字まで使えます" }
+
+  validates :image_file, presence: { message: '画像ファイルを入力してください' }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :photos
   has_secure_password
 
   validates :login_id, presence: true, uniqueness: true
